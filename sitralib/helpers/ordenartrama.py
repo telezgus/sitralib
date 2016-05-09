dfc480b3c2d9dee2bfca5b5f7e7f26ba3747592c
Agustín <agustin.bouillet@gmail.com>
2016-05-09 07:50:13 -0300

    Solución de bug cuando FF llegaba como primer número de la trama

--- sitralib/helpers/ordenartrama.py
+++ sitralib/helpers/ordenartrama.py
@@ -9,7 +9,8 @@ class OrdenarTrama(object):
         trmdict = self.__tramaToDict(trama)
 
         key = self.__getFfPosition(trmdict)
-        if not key: return {}
+
+        if key == None: return {}
         c = 5  # 5 es la posición FF en el protocolo
         trm = {}
         for i in trmdict.keys():
@@ -78,9 +79,14 @@ if __name__ == '__main__':
     o = OrdenarTrama()
     trm1 = o.ordenartrama('00000000FF0000017C000C8E1BBB012F')
     trm2 = o.ordenartrama('00 00 00 00 FF 00 00 01 7C 00 0C 8E 1B BB 01 2F')
-    trm3 = o.ordenartrama('00 00 00 00 FF 00 00 01 C5 00 56 6D 0B B8 00 34 40 20 00 99 99 DD DD DD DD DD DD 16 04 23 15 02 57 07 00 00 00 22 03 00 00 00 05 00 00 00 00 00 14 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 CC')
-    trm4 = o.ordenartrama('00 00 00 00 FF 00 00 01 C8 00 36 00 0B B8 01 14 00 20 00 99 99 DD DD DD DD DD DD 16 04 23 19 21 36 07 00 01 01 02 02 00 00 01 0A 00 16 00 00 00 28 00 00 00 00 00 00 00 00 8B')
-    print(trm1)
-    print(trm2)
-    print(trm3)
-    print(trm4)
+    trm3 = o.ordenartrama('00 00 00 00 00 FF 00 00 01 C5 00 56 6D 0B B8 00 34 40 20 00 99 99 DD DD DD DD DD DD 16 04 23 15 02 57 07 00 00 00 22 03 00 00 00 05 00 00 00 00 00 14 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 CC')
+    trm4 = o.ordenartrama('00 00 FF 00 00 01 C8 00 36 00 0B B8 01 14 00 20 00 99 99 DD DD DD DD DD DD 16 04 23 19 21 36 07 00 01 01 02 02 00 00 01 0A 00 16 00 00 00 28 00 00 00 00 00 00 00 00 8B')
+    trm5 = o.ordenartrama('00 FF 00 00 01 C8 00 36 00 0B B8 01 14 00 20 00 99 99 DD DD DD DD DD DD 16 04 23 19 21 36 07 00 01 01 02 02 00 00 01 0A 00 16 00 00 00 28 00 00 00 00 00 00 00 00 8B')
+    trm6 = o.ordenartrama('00 00 00 00 00 00 00 00 00 00 00 00 FF 00 00 01 C8 00 36 00 0B B8 01 14 00 20 00 99 99 DD DD DD DD DD DD 16 04 23 19 21 36 07 00 01 01 02 02 00 00 01 0A 00 16 00 00 00 28 00 00 00 00 00 00 00 00 8B')
+
+    print(trm1, '\n')
+    print(trm2, '\n')
+    print(trm3, '\n')
+    print(trm4, '\n')
+    print(trm5, '\n')
+    print(trm6)
