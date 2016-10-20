f6506c4e4f7174349a2e99e4da7577173d11b34c
Agustín <agustin.bouillet@gmail.com>
2016-10-19 21:53:14 -0300

    Ventanas modales para mostrar que en el futuro se podrán ver datos.

--- sitralib/respuesta_estrucutra_parte_baja.py
+++ sitralib/respuesta_estrucutra_parte_baja.py
@@ -4,7 +4,7 @@ from sitralib.bits_status_ii import *
 from sitralib.bits_status_iii import *
 from sitralib.byte_status import *
 from sitralib.validators.bcc import *
-
+from sitralib.helpers.funciones import *
 
 class RespuestaEstrucutraParteBaja(object):
     """
@@ -13,6 +13,7 @@ class RespuestaEstrucutraParteBaja(object):
     """
 
     def __init__(self):
+        self.helpers = Helpers()
         self.bytSta = ByteStatus()
         self.bitStaI = BitsStatusI()
         self.bitStaII = BitsStatusII()
@@ -24,13 +25,13 @@ class RespuestaEstrucutraParteBaja(object):
         res = None
 
         if (self.validateBcc.isValidBcc(trm, 12, 20)):
-            res = {'byte_status_a': self.bytSta.byteDeStatus(trm[15])}
+            res = {'byte_status_a': self.bytSta.byteStatus(trm[15])}
             res.update(
                 {'numero_cruce': self.helpers.hexToDec(trm[13] + trm[14])})
-            res.update(self.bitStaI.bitsDeStatusI(trm[16]))
-            res.update(self.bitStaII.bitsDeStatusII(trm[17]))
-            res.update(self.bitAla.bitsDeAlarma(trm[18]))
-            res.update(self.bitStaIII.bitsDeStatusIII(trm[19]))
+            res.update(self.bitStaI.bitsStatusI(trm[16]))
+            res.update(self.bitStaII.bitsStatusII(trm[17]))
+            res.update(self.bitAla.bitsAlarma(trm[18]))
+            res.update(self.bitStaIII.bitsStatusIII(trm[19]))
             res.update({'object': 'RespuestaEstrucutraParteBaja'})
 
         return res
