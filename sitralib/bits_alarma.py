5d270f2dda2050887bb2dff34788dcf7e8bcdc93
Agustin <agustin.bouillet@gmail.com>
2020-05-31 21:21:33 -0300

    Puerta abierta en estado normal no debe mostrarse.

--- sitralib/bits_alarma.py
+++ sitralib/bits_alarma.py
@@ -14,37 +14,40 @@ class BitsAlarma:
 
     byte = Byte(hex)
 
-    evaluacion = [{"val": 0, "des": "Normal"}, {"val": 1, "des": "Falla"},]
+    evaluacion = [
+        {"val": 0, "des": "Normal"},
+        {"val": 1, "des": "Falla"},
+    ]
 
     estado = {
-        "FR": {
-            "des": "Falta de rojo",
-            "est": evaluacion[int(byte.binaryReversed[0])]
-        },
-        "TSUP": {
-            "des": "Tiempo suplementario de ciclo",
-            "est": evaluacion[int(byte.binaryReversed[1])]
-        },
-        "CV": {
-            "des": "Conflicto de verde",
-            "est": evaluacion[int(byte.binaryReversed[2])]
-        },
-        "BT": {
-            "des": "Baja tensión",
-            "est": evaluacion[int(byte.binaryReversed[3])]
-        },
-        "PA": {
-            "des": "Puerta abierta",
-            "est": evaluacion[int(byte.binaryReversed[4])]
-        },
-        "GPS": {
-            "des": "Sistema de posicionamiento global",
-            "est": evaluacion[int(byte.binaryReversed[5])]
-        },
-        "FV": {
-            "des": "Falta de verde",
-            "est": evaluacion[int(byte.binaryReversed[6])]
-        },
+      "FR": {
+        "des": "Falta de rojo",
+        "est": evaluacion[int(byte.binaryReversed[0])]
+      },
+      "TSUP": {
+        "des": "Tiempo suplementario de ciclo",
+        "est": evaluacion[int(byte.binaryReversed[1])]
+      },
+      "CV": {
+        "des": "Conflicto de verde",
+        "est": evaluacion[int(byte.binaryReversed[2])]
+      },
+      "BT": {
+        "des": "Baja tensión",
+        "est": evaluacion[int(byte.binaryReversed[3])]
+      },
+      "PA": {
+        "des": "Puerta abierta",
+        "est": evaluacion[int(byte.binaryReversed[4])]
+      },
+      "GPS": {
+        "des": "Sistema de posicionamiento global",
+        "est": evaluacion[int(byte.binaryReversed[5])]
+      },
+      "FV": {
+        "des": "Falta de verde",
+        "est": evaluacion[int(byte.binaryReversed[6])]
+      },
     }
 
     est = {"bits_alarma": estado}
