500f5391ea6dc637fb950b88fea53c5342e76f86
Agustín Bouillet <agustin.bouillet@gmail.com>
2024-05-10 08:15:48 -0300

    includes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sitralib',
-    version='1.1.82',
+    version='1.1.83',
     packages=find_packages( exclude=['test'] ),
     author='Agustin Bouillet',
     author_email='agustin.bouillet@gmail.com',
